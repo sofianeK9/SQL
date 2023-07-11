bd0610b17df29d2e3fa430fd50c8824a0577fe28
sofianek9 <kayasofiane7@gmail.com>
2023-07-11 16:48:32 +0200

    exercice 04

--- exercice-04.sql
+++ exercice-04.sql
@@ -1,17 +1,43 @@
 -- Exo 4
 -- Requêtes avec jointure « many to one » et « one to many »
-
+SELECT *
+FROM utilisateur
+INNER JOIN groupe ON groupe.id = utilisateur.groupe_id;
 -- Exo 4.1
 -- Listez tous les students avec leurs projects
 -- Note : les students sans projets doivent aussi apparaître dans les résultats
+SELECT student.firstname, student.lastname, project.name,
+FROM `student`
+LEFT OUTER JOIN project
+ON student.project_id = project.id;
 
 -- Exo 4.2
 -- Listez le student dont l'id est `2` avec son project
+SELECT 
+FROM `student`
+INNER JOIN project ON project.id = project_id
+WHERE student.id = 2;
 
 -- Exo 4.3
 -- Listez tous les projects avec leurs students
--- Note : les projets sans studets ne doivent pas apparaître dans les résultats
+-- Note : les projets sans students ne doivent pas apparaître dans les résultats
+    SELECT *
+    FROM project
+    INNER JOIN student
+    ON project.id = student.project_id;
+
+
 
 -- Exo 4.4
 -- Listez le project dont l'id est `3` avec ses students
+SELECT *
+FROM project
+INNER JOIN student ON project_id = project.id
+WHERE student.id = 3
 
+
+
+
+
+-- ceux qui ont un projet : inner join
+-- ceux qui n'ont pas de projet : left outer join
